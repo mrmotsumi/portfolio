3294552bfbabde15e433b9566e9df079e1a5433d
Hunter <hunter4sure911@gmail.com>
2022-07-26 16:07:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-no-undef */
+
+import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    <Testimonials />
+    <Contact />
+  </main>
+
+    
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://media.istockphoto.com/photos/chat-symbol-and-quotation-mark-picture-id517899146?k=20&m=517899146&s=612x612&w=0&h=zvomsvNz0jOYruEic1VUVBBHxreX84NT1t961EWJj3E=",
+      link: "https://schoolsystem.more-xtech.com/",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://media.istockphoto.com/photos/chat-symbol-and-quotation-mark-picture-id517899146?k=20&m=517899146&s=612x612&w=0&h=zvomsvNz0jOYruEic1VUVBBHxreX84NT1t961EWJj3E=",
+      link: "https://schoolsystem.more-xtech.com/",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://media.istockphoto.com/photos/chat-symbol-and-quotation-mark-picture-id517899146?k=20&m=517899146&s=612x612&w=0&h=zvomsvNz0jOYruEic1VUVBBHxreX84NT1t961EWJj3E=",
+      link: "https://schoolsystem.more-xtech.com/",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "https://media.istockphoto.com/photos/chat-symbol-and-quotation-mark-picture-id517899146?k=20&m=517899146&s=612x612&w=0&h=zvomsvNz0jOYruEic1VUVBBHxreX84NT1t961EWJj3E=",
+      link: "https://schoolsystem.more-xtech.com/",
+    },
+  ];
+
+
+
+  export const skills = [
+    {
+      skill: ".Net Development (C#)",
+      image: "./project-2.gif",
+      company: "Company X",
+      
+    },
+    {
+      skill: "Java EE (Spring Boot) / SE",
+      image: "https://popupsmart.com/encyclopedia/images/t/e/s/t/i/testimonial-example-da3e2204.png",
+      company: "Company X",
+      name: "",
+      
+    },
+    {
+      skill: "Web Development (React js)",
+      image: "./project-2.gif",
+      company: "Company X",
+      
+    },
+    {
+      skill: "Web Development (React js)",
+      image: "./project-2.gif",
+      
+    },
+  ];
+
+
+  
+  export const testimonials = [
+    {
+      quote: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+      company: "Company X",
+      image: "https://popupsmart.com/encyclopedia/images/t/e/s/t/i/testimonial-example-da3e2204.png",
+
+    },
+    {
+      quote: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      company: "Company X",
+      image: "https://popupsmart.com/encyclopedia/images/t/e/s/t/i/testimonial-example-da3e2204.png",
+
+    },
+    {
+      quote: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      company: "Company X",
+      image: "https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720/76729750.jpg",
+
+    },
+    {
+      quote: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+      company: "Company X",
+      image: "https://popupsmart.com/encyclopedia/images/t/e/s/t/i/testimonial-example-da3e2204.png",
+
+    },
+  ];
